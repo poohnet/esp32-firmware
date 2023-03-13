3bad5906bb6e7cabba0bff0c56b256ffda2e16c9
Thomas Hein <github@poohnet.de>
2023-03-13 08:05:11 +0100

    Beschreibung der OBIS-Kennzahlen hinzugefügt

--- software/src/modules/sem20/sem20.cpp
+++ software/src/modules/sem20/sem20.cpp
@@ -17,68 +17,72 @@ unsigned int mc_Port = 9522;
 
 
 obis kennzahlen[] = {
-  obis(0,  1, 4, 0,      10.0, 4),
-  obis(0,  1, 8, 0, 3600000.0, 8),
-  obis(0,  2, 4, 0,      10.0, 4),
-  obis(0,  2, 8, 0, 3600000.0, 8),
-  obis(0,  3, 4, 0,      10.0, 4),
-  obis(0,  3, 8, 0, 3600000.0, 8),
-  obis(0,  4, 4, 0,      10.0, 4),
-  obis(0,  4, 8, 0, 3600000.0, 8),
-  obis(0,  9, 4, 0,      10.0, 4),
-  obis(0,  9, 8, 0, 3600000.0, 8),
-  obis(0, 10, 4, 0,      10.0, 4),
-  obis(0, 10, 8, 0, 3600000.0, 8),
-  obis(0, 13, 4, 0,    1000.0, 4),
-  obis(0, 14, 4, 0,    1000.0, 4),
+  // Summen
+  obis(0,  1, 4, 0,      10.0, 4),  // Wirkleistung/-arbeit + (aktueller Mittelwert)
+  obis(0,  1, 8, 0, 3600000.0, 8),  // Wirkleistung/-arbeit + (Zählerstand)
+  obis(0,  2, 4, 0,      10.0, 4),  // Wirkleistung/-arbeit - (aktueller Mittelwert)
+  obis(0,  2, 8, 0, 3600000.0, 8),  // Wirkleistung/-arbeit - (Zählerstand)
+  obis(0,  3, 4, 0,      10.0, 4),  // Blindleistung/-arbeit + (aktueller Mittelwert)
+  obis(0,  3, 8, 0, 3600000.0, 8),  // Blindleistung/-arbeit + (Zählerstand)
+  obis(0,  4, 4, 0,      10.0, 4),  // Blindleistung/-arbeit - (aktueller Mittelwert)
+  obis(0,  4, 8, 0, 3600000.0, 8),  // Blindleistung/-arbeit - (Zählerstand)
+  obis(0,  9, 4, 0,      10.0, 4),  // Scheinleistung/-arbeit + (aktueller Mittelwert)
+  obis(0,  9, 8, 0, 3600000.0, 8),  // Scheinleistung/-arbeit + (Zählerstand)
+  obis(0, 10, 4, 0,      10.0, 4),  // Scheinleistung/-arbeit - (aktueller Mittelwert)
+  obis(0, 10, 8, 0, 3600000.0, 8),  // Scheinleistung/-arbeit - (Zählerstand)
+  obis(0, 13, 4, 0,    1000.0, 4),  // Leistungsfaktor (cos phi)
+  obis(0, 14, 4, 0,    1000.0, 4),  // Netzfrequenz
 
-  obis(0, 21, 4, 0,      10.0, 4),
-  obis(0, 21, 8, 0, 3600000.0, 8),
-  obis(0, 22, 4, 0,      10.0, 4),
-  obis(0, 22, 8, 0, 3600000.0, 8),
-  obis(0, 23, 4, 0,      10.0, 4),
-  obis(0, 23, 8, 0, 3600000.0, 8),
-  obis(0, 24, 4, 0,      10.0, 4),
-  obis(0, 24, 8, 0, 3600000.0, 8),
-  obis(0, 29, 4, 0,      10.0, 4),
-  obis(0, 29, 8, 0, 3600000.0, 8),
-  obis(0, 30, 4, 0,      10.0, 4),
-  obis(0, 30, 8, 0, 3600000.0, 8),
-  obis(0, 31, 4, 0,    1000.0, 4),
-  obis(0, 32, 4, 0,    1000.0, 4),
-  obis(0, 33, 4, 0,    1000.0, 4),
+  // Phase 1
+  obis(0, 21, 4, 0,      10.0, 4),  // Wirkleistung/-arbeit + (aktueller Mittelwert)
+  obis(0, 21, 8, 0, 3600000.0, 8),  // Wirkleistung/-arbeit + (Zählerstand)
+  obis(0, 22, 4, 0,      10.0, 4),  // Wirkleistung/-arbeit - (aktueller Mittelwert)
+  obis(0, 22, 8, 0, 3600000.0, 8),  // Wirkleistung/-arbeit - (Zählerstand)
+  obis(0, 23, 4, 0,      10.0, 4),  // Blindleistung/-arbeit + (aktueller Mittelwert)
+  obis(0, 23, 8, 0, 3600000.0, 8),  // Blindleistung/-arbeit + (Zählerstand)
+  obis(0, 24, 4, 0,      10.0, 4),  // Blindleistung/-arbeit - (aktueller Mittelwert)
+  obis(0, 24, 8, 0, 3600000.0, 8),  // Blindleistung/-arbeit - (Zählerstand)
+  obis(0, 29, 4, 0,      10.0, 4),  // Scheinleistung/-arbeit + (aktueller Mittelwert)
+  obis(0, 29, 8, 0, 3600000.0, 8),  // Scheinleistung/-arbeit + (Zählerstand)
+  obis(0, 30, 4, 0,      10.0, 4),  // Scheinleistung/-arbeit - (aktueller Mittelwert)
+  obis(0, 30, 8, 0, 3600000.0, 8),  // Scheinleistung/-arbeit - (Zählerstand)
+  obis(0, 31, 4, 0,    1000.0, 4),  // Strom
+  obis(0, 32, 4, 0,    1000.0, 4),  // Spannung
+  obis(0, 33, 4, 0,    1000.0, 4),  // Leistungsfaktor (cos phi)
 
-  obis(0, 41, 4, 0,      10.0, 4),
-  obis(0, 41, 8, 0, 3600000.0, 8),
-  obis(0, 42, 4, 0,      10.0, 4),
-  obis(0, 42, 8, 0, 3600000.0, 8),
-  obis(0, 43, 4, 0,      10.0, 4),
-  obis(0, 43, 8, 0, 3600000.0, 8),
-  obis(0, 44, 4, 0,      10.0, 4),
-  obis(0, 44, 8, 0, 3600000.0, 8),
-  obis(0, 49, 4, 0,      10.0, 4),
-  obis(0, 49, 8, 0, 3600000.0, 8),
-  obis(0, 50, 4, 0,      10.0, 4),
-  obis(0, 50, 8, 0, 3600000.0, 8),
-  obis(0, 51, 4, 0,    1000.0, 4),
-  obis(0, 52, 4, 0,    1000.0, 4),
-  obis(0, 53, 4, 0,    1000.0, 4),
+  // Phase 2
+  obis(0, 41, 4, 0,      10.0, 4),  // Wirkleistung/-arbeit + (aktueller Mittelwert)
+  obis(0, 41, 8, 0, 3600000.0, 8),  // Wirkleistung/-arbeit + (Zählerstand)
+  obis(0, 42, 4, 0,      10.0, 4),  // Wirkleistung/-arbeit - (aktueller Mittelwert)
+  obis(0, 42, 8, 0, 3600000.0, 8),  // Wirkleistung/-arbeit - (Zählerstand)
+  obis(0, 43, 4, 0,      10.0, 4),  // Blindleistung/-arbeit + (aktueller Mittelwert)
+  obis(0, 43, 8, 0, 3600000.0, 8),  // Blindleistung/-arbeit + (Zählerstand)
+  obis(0, 44, 4, 0,      10.0, 4),  // Blindleistung/-arbeit - (aktueller Mittelwert)
+  obis(0, 44, 8, 0, 3600000.0, 8),  // Blindleistung/-arbeit - (Zählerstand)
+  obis(0, 49, 4, 0,      10.0, 4),  // Scheinleistung/-arbeit + (aktueller Mittelwert)
+  obis(0, 49, 8, 0, 3600000.0, 8),  // Scheinleistung/-arbeit + (Zählerstand)
+  obis(0, 50, 4, 0,      10.0, 4),  // Scheinleistung/-arbeit - (aktueller Mittelwert)
+  obis(0, 50, 8, 0, 3600000.0, 8),  // Scheinleistung/-arbeit - (Zählerstand)
+  obis(0, 51, 4, 0,    1000.0, 4),  // Strom
+  obis(0, 52, 4, 0,    1000.0, 4),  // Spannung
+  obis(0, 53, 4, 0,    1000.0, 4),  // Leistungsfaktor (cos phi)
 
-  obis(0, 61, 4, 0,      10.0, 4),
-  obis(0, 61, 8, 0, 3600000.0, 8),
-  obis(0, 62, 4, 0,      10.0, 4),
-  obis(0, 62, 8, 0, 3600000.0, 8),
-  obis(0, 63, 4, 0,      10.0, 4),
-  obis(0, 63, 8, 0, 3600000.0, 8),
-  obis(0, 64, 4, 0,      10.0, 4),
-  obis(0, 64, 8, 0, 3600000.0, 8),
-  obis(0, 69, 4, 0,      10.0, 4),
-  obis(0, 69, 8, 0, 3600000.0, 8),
-  obis(0, 70, 4, 0,      10.0, 4),
-  obis(0, 70, 8, 0, 3600000.0, 8),
-  obis(0, 71, 4, 0,    1000.0, 4),
-  obis(0, 72, 4, 0,    1000.0, 4),
-  obis(0, 73, 4, 0,    1000.0, 4),
+  // Phase 3
+  obis(0, 61, 4, 0,      10.0, 4),  // Wirkleistung/-arbeit + (aktueller Mittelwert)
+  obis(0, 61, 8, 0, 3600000.0, 8),  // Wirkleistung/-arbeit + (Zählerstand)
+  obis(0, 62, 4, 0,      10.0, 4),  // Wirkleistung/-arbeit - (aktueller Mittelwert)
+  obis(0, 62, 8, 0, 3600000.0, 8),  // Wirkleistung/-arbeit - (Zählerstand)
+  obis(0, 63, 4, 0,      10.0, 4),  // Blindleistung/-arbeit + (aktueller Mittelwert)
+  obis(0, 63, 8, 0, 3600000.0, 8),  // Blindleistung/-arbeit + (Zählerstand)
+  obis(0, 64, 4, 0,      10.0, 4),  // Blindleistung/-arbeit - (aktueller Mittelwert)
+  obis(0, 64, 8, 0, 3600000.0, 8),  // Blindleistung/-arbeit - (Zählerstand)
+  obis(0, 69, 4, 0,      10.0, 4),  // Scheinleistung/-arbeit + (aktueller Mittelwert)
+  obis(0, 69, 8, 0, 3600000.0, 8),  // Scheinleistung/-arbeit + (Zählerstand)
+  obis(0, 70, 4, 0,      10.0, 4),  // Scheinleistung/-arbeit - (aktueller Mittelwert)
+  obis(0, 70, 8, 0, 3600000.0, 8),  // Scheinleistung/-arbeit - (Zählerstand)
+  obis(0, 71, 4, 0,    1000.0, 4),  // Strom
+  obis(0, 72, 4, 0,    1000.0, 4),  // Spannung
+  obis(0, 73, 4, 0,    1000.0, 4),  // Leistungsfaktor (cos phi)
 };
 
 
