9de882dd6afc4c9267c97d2ec51e0bc7325923f5
Thomas Hein <github@poohnet.de>
2023-07-30 19:06:19 +0200

    Module IndustrialQuadRelay

---
+++ software/src/modules/industrial_quad_relay/industrial_quad_relay.cpp
@@ -0,0 +1,75 @@
+/* esp32-firmware
+ * Copyright (C) 2020-2021 Erik Fleckstein <erik@tinkerforge.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "industrial_quad_relay.h"
+
+#include "bindings/errors.h"
+
+#include "api.h"
+#include "event_log.h"
+#include "tools.h"
+#include "task_scheduler.h"
+#include "modules.h"
+
+extern EventLog logger;
+
+extern TF_HAL hal;
+extern TaskScheduler task_scheduler;
+
+extern API api;
+
+
+IndustrialQuadRelay::IndustrialQuadRelay()
+  : DeviceModule("industrial_quad_relay", "Industrial Quad Relay", "Industrial Quad Relay", std::bind(&IndustrialQuadRelay::setup_IndustrialQuadRelay, this))
+{
+}
+
+IndustrialQuadRelay::~IndustrialQuadRelay()
+{
+}
+
+void IndustrialQuadRelay::pre_setup()
+{
+}
+
+void IndustrialQuadRelay::setup_IndustrialQuadRelay()
+{
+  if (!this->DeviceModule::setup_device()) {
+    return;
+  }
+}
+
+void IndustrialQuadRelay::setup()
+{
+  setup_IndustrialQuadRelay();
+
+  if (!device_found) {
+    return;
+  }
+}
+
+void IndustrialQuadRelay::register_urls()
+{
+  this->DeviceModule::register_urls();
+}
+
+void IndustrialQuadRelay::loop()
+{
+  this->DeviceModule::loop();
+}
