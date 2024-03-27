81c2e67c1184cf2009da917534d284d53fdde01c
Thomas Hein <github@poohnet.de>
2024-03-27 16:44:22 +0100

    feat(EVSE CPC, Phase Switcher): Use Control Pilot Backend

--- software/src/modules/evse_cpc/evse_cpc.h
+++ software/src/modules/evse_cpc/evse_cpc.h
@@ -19,13 +19,12 @@
 
 #pragma once
 
+#include "device_module.h"
 #include "config.h"
-#include "module.h"
 
 #include "bindings/bricklet_evse_cpc.h"
 
-#include "device_module.h"
-#include "evse_cpc_bricklet_firmware_bin.embedded.h"
+#include "../evse/control_pilot_backend.h"
 
 #if defined(__GNUC__)
   #pragma GCC diagnostic push
@@ -34,13 +33,11 @@
 #endif
 
 class EvseCPC final : public DeviceModule<TF_EVSECPC,
-                                evse_cpc_bricklet_firmware_bin_data,
-                                evse_cpc_bricklet_firmware_bin_length,
                                 tf_evse_cpc_create,
                                 tf_evse_cpc_get_bootloader_mode,
                                 tf_evse_cpc_reset,
                                 tf_evse_cpc_destroy,
-                                false>
+                                false>, ControlPilotBackend
 {
 public:
   EvseCPC();
@@ -52,8 +49,9 @@ public:
 
   void setup_evse_cpc();
 
-  bool get_control_pilot_disconnect();
-  void set_control_pilot_disconnect(bool cp_disconnect, bool* cp_disconnected);
+  // ControlPilotBackend implementation
+  bool get_control_pilot_disconnect() const override;
+  void set_control_pilot_disconnect(bool cp_disconnect, bool* cp_disconnected) override;
 
 private:
   void update_all_data();
