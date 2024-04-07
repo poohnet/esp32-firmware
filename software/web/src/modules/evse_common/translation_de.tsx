244ea1b4674847f503b85adf8b61a254ef321e6e
Thomas Hein <github@poohnet.de>
2024-04-07 18:50:45 +0200

    feat(WARP on Steroids): Use Phase Switcher Bricklet

--- software/web/src/modules/evse_common/translation_de.tsx
+++ software/web/src/modules/evse_common/translation_de.tsx
@@ -134,7 +134,8 @@ let x = {
                 11: "OCPP",
                 12: "Energie-/Zeitlimit",
                 13: "Zählerüberwachung",
-                14: "Automatisierung"
+                14: "Automatisierung",
+                15: "Phase Switcher"
             }[i];}/*NF*/,
 
             // EVSE V1 only
@@ -322,7 +323,8 @@ let x = {
                 11: "OCPP",
                 12: "Energie-/Zeitlimit",
                 13: "Zählerüberwachung",
-                14: "Automation"
+                14: "Automatisierung",
+                15: "Phase Switcher"
             }[i];}/*NF*/,
 
             "reboot_content_changed": "Ladeeinstellungen",
